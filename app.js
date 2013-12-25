2f6fa9c27d3458f505b23831a21961eb045c7bea
Brooke Osborn <dsullivan7@hotmail.com>
2013-12-25 13:09:54 -0500

    adding views

---
+++ app.js
@@ -0,0 +1,27 @@
+var move = require('./lib/move');
+var judge = require('./lib/judge');
+var path = require('path');
+var express = require('express');
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/', function(req,res){
+    res.render('index');
+});
+
+app.get('/move', function(req,res){
+    var board = JSON.parse(req.query.board);
+    var nextMove = move.makeMove(board);
+    res.send(200, nextMove);
+});
+
+app.get('/judge', function(req, res){
+    var board = JSON.parse(req.query.board);
+    var winner = judge.judge(board);
+    res.send(200, winner); 
+});
+
+app.listen(3000);
--- test.js
+++ test.js
@@ -1,11 +1,11 @@
-var tictac = require('./validator.js');
+var tictac = require('./lib/move');
 var x = 'x';
 var o = 'o';
 var myArray = 
 [
-[o,o,o],
-[o,o,x],
+[null,o,o],
+[o,null,o],
 [x,x,x]
-]
+];
 
-console.log(tictac.validate(myArray));
+console.log(tictac.makeMove(myArray));
